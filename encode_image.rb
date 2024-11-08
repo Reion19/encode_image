49f673d99764f998c69bd6ba6ec8f5fef3b81af9
Reion19 <reizu1906@gmail.com>
2024-11-08 10:58:37 +0000

    Initial commit

--- encode_image.rb
+++ encode_image.rb
@@ -34,8 +34,8 @@ module ProcessHandler
   def self.handle_upload(args)
     raise UnsupportedParam if ARGUMENTS_ARRAY.include? ARGUMENTS_ARRAY - args.keys
     raise NoParamsError if args.keys == []
-    raise BlankString if BASE64_STRING.nil? && args.key?("b")
-    raise BlankPath if PATH.nil? && (args.key?("p") || args.key?("path"))
+    raise BlankString if BASE64_STRING.nil? && args.key?('b')
+    raise BlankPath if PATH.nil? && (args.key?('p') || args.key?('path'))
 
     encode if ENCODE_PARAMS && PATH
     decode(args) if DECODE_PARAMS && BASE64_STRING
@@ -43,23 +43,23 @@ module ProcessHandler
   end
 end
 
-require "base64"
-args = Hash[ARGV.join(" ").scan(/--?([^=\s]+)(?:=(\S+))?/)]
+require 'base64'
+args = Hash[ARGV.join(' ').scan(/--?([^=\s]+)(?:=(\S+))?/)]
 ARGUMENTS_ARRAY = %w[h help e encode d decode b p path f filename].freeze
-BINARY_NAME = File.extname(__FILE__) == "" ? File.basename(__FILE__) : __FILE__
-ENCODE_PARAMS = args.key?("encode") || args.key?("e")
-DECODE_PARAMS = args.key?("decode") || args.key?("d")
-HELP_PARAMS = args.key?("help") || args.key?("h")
-FILENAME_PARAMS = args["filename"] || args["f"]
-PATH = args["path"] || args["p"]
-BASE64_STRING = args["b"]
+BINARY_NAME = File.extname(__FILE__) == '' ? File.basename(__FILE__) : __FILE__
+ENCODE_PARAMS = args.key?('encode') || args.key?('e')
+DECODE_PARAMS = args.key?('decode') || args.key?('d')
+HELP_PARAMS = args.key?('help') || args.key?('h')
+FILENAME_PARAMS = args['filename'] || args['f']
+PATH = args['path'] || args['p']
+BASE64_STRING = args['b']
 SUPPORTED_TYPES = %w[png jpg jpeg gif rb php html].freeze
-DEFAULT_NAME = "test_name"
+DEFAULT_NAME = 'test_name'
 STANDARD_OUTPUT = "Please, type `#{BINARY_NAME} -h` or `#{BINARY_NAME} --help` for help"
 USAGE = <<ENDUSAGE
   #{
-  if File.extname(__FILE__) != ""
-    "For more experience type `chmod +x #{__FILE__} && mv #{__FILE__} /usr/local/bin && #{File.basename(__FILE__, ".*")}`"
+  if File.extname(__FILE__) != ''
+    "For more experience type `chmod +x #{__FILE__} && mv #{__FILE__} /usr/local/bin && #{File.basename(__FILE__, '.*')}`"
   end}
   Usage:
     #{BINARY_NAME} [-h] [decode [-d][--decode] [--path=PATH]] [encode [-e][--encode] [-b=BASE64] [-f=FILENAME][--filename=FILENAME]]
@@ -81,11 +81,11 @@ HELP = <<ENDHELP
         #{BINARY_NAME} -e --path=#{Dir.home}/foobar.png #=> SGVsbG8sIHdvcmxkIQ==
         #{BINARY_NAME} -d -b=SGVsbG8sIHdvcmxkIQ==        #=> File has been stored at #{Dir.pwd}/foobar.png
     Supported filetypes:
-        #{SUPPORTED_TYPES.join ", "}
+        #{SUPPORTED_TYPES.join ', '}
 ENDHELP
 
 def encode
-  base64_file = File.open(PATH, "rb") do |file|
+  base64_file = File.open(PATH, 'rb') do |file|
     Base64.strict_encode64(file.read)
   end
   print base64_file
@@ -94,17 +94,16 @@ end
 def decode(args)
   img_from_base64 = Base64.decode64(BASE64_STRING)
   img_from_base64[0, 8]
-  binding.irb
   filetype = (/(png|jpg|jpeg|jfif|gif|rb|php|html)/i.match(img_from_base64[0, 16])[0]).downcase
   filetype = case filetype
              when 'jfif' then 'jpeg'
              else
                filetype
              end
-  filename = !FILENAME_PARAMS.nil? ? args["filename"] || args["f"] : DEFAULT_NAME
+  filename = !FILENAME_PARAMS.nil? ? args['filename'] || args['f'] : DEFAULT_NAME
   file = "#{filename}.#{filetype}"
 
-  File.open(file, "wb") do |f|
+  File.open(file, 'wb') do |f|
     f.write(img_from_base64)
   end
 
